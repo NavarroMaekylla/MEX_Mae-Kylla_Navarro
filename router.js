14e955def3db697b494c2719054bf689d03bd420
MAE KYLLA NAVARRO <1383-19@itmlyceumalabang.onmicrosoft.com>
2022-05-07 21:58:34 +0800

    add file

---
+++ router.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var router = express.Router();
+var bodyParser = require("body-parser");
+
+
+const credential = {
+    username: "admin",
+    password: "admin1234"
+}
+
+// login user
+router.post('/login', (req, res) => {
+    if (req.body.username == credential.username && req.body.password == credential.password) {
+        req.session.user = req.body.username;
+        res.redirect('/route/dashboard');
+        //res.end("Login Successful...!");
+    } else {
+        res.end("Invalid Username")
+    }
+});
+
+// route for dashboard
+router.get('/dashboard', (req, res) => {
+    if (req.session.user) {
+        res.render('dashboard', { user: req.session.user })
+    } else {
+        res.send("Unauthorize User")
+    }
+})
+
+// route for logout
+router.get('/logout', (req, res) => {
+    req.session.destroy(function(err) {
+        if (err) {
+            console.log(err);
+            res.send("Error")
+        } else {
+            res.render('base', { title: "Express", logout: "Logout Successfully...!" })
+        }
+    })
+})
+
+module.exports = router;
